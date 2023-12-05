367da3c9f3e65880b62c9bf591a79c5e94a286af
mikoyekel13 <mikoyekel@hilma.tech>
2023-12-05 10:43:05 +0200

    idk

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var teacherRouter = require("./routes/teacher");
+var classroomRouter = require("./routes/classroom");
+var studentRouter = require("./routes/student");
+var schoolRouter = require("./routes/school");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/classroom", classroomRouter);
+app.use("/teacher", teacherRouter);
+app.use("/student", studentRouter);
+app.use("/school", schoolRouter);
 
 module.exports = app;
